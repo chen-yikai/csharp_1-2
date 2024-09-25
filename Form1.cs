e64a112f967398fcaa3820d9a2b43d143eccda19
EliasChen <eliaschen.dev@gmail.com>
2024-09-25 11:58:32 +0800

    add comment

--- Form1.cs
+++ Form1.cs
@@ -21,49 +21,55 @@ namespace _1_2
 
         private void showCurrentDate(object sender, EventArgs e)
         {
-            masterLabel.Text = DateTime.Now.ToString();
+            masterLabel.Text = DateTime.Now.ToString(); //顯示現在時間
         }
 
         private void showText_Click(object sender, EventArgs e)
         {
-            masterLabel.Text = "HelloWorld!";
+            masterLabel.Text = "HelloWorld!"; //顯示文字
         }
 
+        // 文字對齊
         private void left_Click(object sender, EventArgs e)
         {
-            masterLabel.TextAlign = ContentAlignment.MiddleLeft;
+            masterLabel.TextAlign = ContentAlignment.MiddleLeft; //靠左
         }
 
         private void center_Click(object sender, EventArgs e)
         {
-            masterLabel.TextAlign = ContentAlignment.MiddleCenter;
+            masterLabel.TextAlign = ContentAlignment.MiddleCenter; //置中
         }
 
         private void right_Click(object sender, EventArgs e)
         {
-            masterLabel.TextAlign = ContentAlignment.MiddleRight;
+            masterLabel.TextAlign = ContentAlignment.MiddleRight; //靠右
         }
+        //
 
         private void greenbg_Click(object sender, EventArgs e)
         {
-            masterLabel.BackColor = Color.Green;
+            masterLabel.BackColor = Color.Green; //文字背景綠色
         }
 
         private void lightBlueText_Click(object sender, EventArgs e)
         {
-            masterLabel.ForeColor = Color.LightBlue;
+            masterLabel.ForeColor = Color.LightBlue; //文字顏色淺藍色
         }
 
         private void formColor_Click(object sender, EventArgs e)
         {
-            this.BackColor = Color.Yellow;
+            this.BackColor = Color.Yellow; //表單背景黃色
         }
 
         private void clearAllColor_Click(object sender, EventArgs e)
         {
+            //// 恢復預設值
+            //文字
             this.BackColor = SystemColors.Control;
             masterLabel.ForeColor= SystemColors.ControlText;
+            //表單
             masterLabel.BackColor= SystemColors.Control;
+            ////
         }
     }
 }
